f968a8907ce3456be3a9eef10813d3d1a3d70b0e
bigbyrus <cyrocp7.cg@gmail.com>
2025-04-02 18:02:42 -0700

    Updated main, configured I2C pins

--- main/motr_ctrl.c
+++ main/motr_ctrl.c
@@ -1,39 +1,15 @@
 #include <stdio.h>
 #include "driver/ledc.h"
 #include "driver/gpio.h"
+#include "driver/i2c_master.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "hal/adc_types.h"
-#include "esp_adc/adc_oneshot.h"
-
+#include "esp_log.h"
 /* 
 *  NOW:   FIGURE OUT how i will implement IMU / pid loop 
 */
 
-static const char *TAG = "JOYSTICK";
-QueueHandle_t joystick_queue;
-adc_oneshot_unit_handle_t adc1_handle;
-
-typedef struct {
-    int x_value;
-    int y_value;
-} joystick_data_t;
-
-void joystick_task(void *arg) {
-    joystick_data_t joystick;
-    while (1) {
-        joystick.x_value = adc_oneshot_read(adc1_handle, ADC_CHANNEL_0, &joystick.x_value);
-        joystick.y_value = adc_oneshot_read(adc1_handle, ADC_CHANNEL_3, &joystick.y_value);
-
-        // Send data to queue (non-blocking)
-        if (xQueueSend(joystick_queue, &joystick, 10) == pdPASS) {
-            ESP_LOGI(TAG, "Joystick X: %d, Y: %d", joystick.x_value, joystick.y_value);
-        }
-
-        // Read joystick every 100ms
-        vTaskDelay(pdMS_TO_TICKS(100));  
-    }
-}
+static const char *TAG = "init";
 
 // Set motor direction
 void set_motor_direction(int motor, bool forward) {
@@ -57,7 +33,7 @@ void set_motor_speed(int motor, uint32_t speed) {
     }
 }
 
-void init(){
+static void motor_init(){
     // set LEDC timer 
     ledc_timer_config_t time0 = {
         .speed_mode = LEDC_HIGH_SPEED_MODE,
@@ -66,7 +42,7 @@ void init(){
         .freq_hz = 10000,
         .clk_cfg = LEDC_AUTO_CLK,
     };
-    ledc_timer_config(&time0);
+    ESP_ERROR_CHECK(ledc_timer_config(&time0));
     // set LEDC channel0 (PWM1)
     ledc_channel_config_t chan0 = {
         .gpio_num = GPIO_NUM_18,
@@ -77,7 +53,7 @@ void init(){
         .duty = 0,
         .hpoint = 0,
     };
-    ledc_channel_config(&chan0);
+    ESP_ERROR_CHECK(ledc_channel_config(&chan0));
     // set LEDC channel1 (PWM2)
     ledc_channel_config_t chan1 = {
         .gpio_num = GPIO_NUM_19,
@@ -88,7 +64,7 @@ void init(){
         .duty = 0,
         .hpoint = 0,
     };
-    ledc_channel_config(&chan1);
+    ESP_ERROR_CHECK(ledc_channel_config(&chan1));
     // configure GPIO pins
     gpio_config_t gp0 = {
         .pin_bit_mask = (1ULL<<10),
@@ -118,31 +94,38 @@ void init(){
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_DISABLE
     };
-    gpio_config(&gp0);
-    gpio_config(&gp1);
-    gpio_config(&gp2);
-    gpio_config(&gp3);
+    ESP_ERROR_CHECK(gpio_config(&gp0));
+    ESP_ERROR_CHECK(gpio_config(&gp1));
+    ESP_ERROR_CHECK(gpio_config(&gp2));
+    ESP_ERROR_CHECK(gpio_config(&gp3));
     gpio_dump_io_configuration(stdout, (1ULL << 10) | (1ULL << 11) | (1ULL << 12) | (1ULL << 13));
-
-
-    /* only one ADC unit needed for the two GPIO pins reading analog signals */
-    adc_oneshot_unit_init_cfg_t init_config1 = {
-        .unit_id = ADC_UNIT_1,
-        .ulp_mode = ADC_ULP_MODE_DISABLE
-    };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
-    adc_oneshot_chan_cfg_t config = {
-        .bitwidth = ADC_BITWIDTH_DEFAULT,
-        .atten = ADC_ATTEN_DB_12,
-    };
-    /* configure GPIO 39 and GPIO 36 to act as ADC */
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_0, &config));
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_3, &config));
 }
 
-/* Sending joystick data to queue every 100ms */
+static void i2c_init(i2c_master_bus_handle_t *bus_handle, i2c_master_dev_handle_t *device_handle){
+    i2c_master_bus_config_t bus_config = {
+        .i2c_port = I2C_NUM_0,
+        .sda_io_num = GPIO_NUM_21,
+        .scl_io_num = GPIO_NUM_22,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+        .flags.enable_internal_pullup = true
+    };
+    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, bus_handle));
+
+    i2c_device_config_t device_config = {
+        .dev_addr_length = I2C_ADDR_BIT_7,
+        .device_address = 0x67,
+        .scl_speed_hz = 10000
+    };
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(*bus_handle, &device_config, device_handle));
+}
+
+/* Attempting to read IMU measurement for processing */
 void app_main(void)
 {
-    init();
-    xTaskCreate(joystick_task, "joystck_task", 2048, NULL, 5, NULL);
+    motor_init();
+    ESP_LOGI(TAG, "LEDC & GPIO pins initialized");
+
+    
+    //xTaskCreate(joystick_task, "joystck_task", 2048, NULL, 5, NULL);
 }
